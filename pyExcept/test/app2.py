02fb16f12ca804fca8075a1a659f237b83e1e851
simonqianvip <simonqian_vip@163.com>
2016-10-11 15:35:25 +0800

    添加test包

--- pyExcept/test/app2.py
+++ pyExcept/test/app2.py
@@ -1,15 +1,22 @@
 # -*- coding:utf-8 -*-
 import logging
 
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-fh = logging.FileHandler('log/app2.log')
+fh = logging.FileHandler('../log/app2.log')
 fh.setLevel(logging.DEBUG)
 logger.addHandler(fh)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 
+
+class App2:
+    def app_test(self):
+        print 'aaa'
+        logger.info('aa')
+
+
 if __name__ == '__main__':
-    print 'aaa'
-    logger.info('aa')
-    print 'bb'
+    app_ = App2()
+    app_.app_test()
